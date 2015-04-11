b4cb8c49544fe4be2a0a33d3e399cc3b4a9476a6
Andrew Suneson <ajsuneso@c28-0117-01.ad.mtu.edu>
2015-04-11 10:49:52 -0400

    Updated the file to spawn

--- Competitor/src/competitor/CompetitorAI.java
+++ Competitor/src/competitor/CompetitorAI.java
@@ -1,19 +1,52 @@
 package competitor;
 
-import snowbound.api.*;
+import java.util.Set;
 
-@Agent(name = "AIName")
+import snowbound.api.*;
+import snowbound.api.util.*;
+import static snowbound.api.util.Utility.*;
+
+@Agent(name = "Tundra Wookie Slayers")
 public class CompetitorAI extends AI{
-	
+
+	Set<Position> spawns = null;
+
 	public CompetitorAI(){
-		
+
 	}
-	
+
 	/**
 	 *
 	 **/
 	public Action action(Turn turn) {
-		return null;
+
+		// Setup
+		if (spawns == null){
+			spawns = turn.myTeam().spawns();
+		}
+
+		Unit currentUnit = turn.actor();
+
+		if (!currentUnit.isSpawned()){
+			for (Position possibleSpot: spawns){
+				if (!turn.hasUnitAt(possibleSpot)){
+					// Use Possible set
+					return new SpawnAction(possibleSpot, Perk.CLEATS);
+				}
+			}
+			// No spawn points available.
+			try {
+				throw new Exception("Could not spawn");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}else{
+			return null;
+		}
+
+
 	}
 
 }
