af42e332cfc19965f243c6d7e70bb1e2106a4b36
Andrew Suneson <ajsuneso@c28-0117-01.ad.mtu.edu>
2015-04-11 13:24:48 -0400

    The goals now reflect where the players want to go.

--- Competitor/src/competitor/CompetitorAI.java
+++ Competitor/src/competitor/CompetitorAI.java
@@ -56,21 +56,21 @@ public class CompetitorAI extends AI{
 			int numSpawned = 0;
 
 			// Calculate how many guys we have spanwed
-			for (Unit unit: turn.myUnits()){
+			/*for (Unit unit: turn.myUnits()){
 				if (unit.isSpawned()){
 					numSpawned ++;
 				}
 			}
-			System.out.println(numSpawned);
+			System.out.println(numSpawned);*/
 
 			// Find the correct perk
 			Perk newPerk;
 
 			// TODO Wont work if a unit is killed
-			if (numSpawned == numUnitsPerTeam-1 && numUnitsPerTeam > 3){
+			if (unit_id == numUnitsPerTeam-1 && numUnitsPerTeam > 3){
 				newPerk = Perk.LAYERS; // One Layer member for our team if we have more than 3
 				System.out.println("Layers");
-			}else if (numSpawned == numUnitsPerTeam - 2 && numUnitsPerTeam > 3 || numSpawned == numUnitsPerTeam -1 && numUnitsPerTeam <= 3){
+			}else if (unit_id == numUnitsPerTeam - 2 && numUnitsPerTeam > 3 || unit_id == numUnitsPerTeam -1 && numUnitsPerTeam <= 3){
 				newPerk = Perk.BUCKET;
 				System.out.println("Bucket");
 			}else{
@@ -82,7 +82,8 @@ public class CompetitorAI extends AI{
 			Set<Base> notOurBases = difference(bases, turn.myBases()); 
 
 			int shortestDistance = 100000000;
-			int farthestDistance = 0;
+			int farthestDistance = 100000000;
+			Base target = null;
 			Position targetSpawn = null;
 
 			// For all possible spawns
@@ -97,23 +98,33 @@ public class CompetitorAI extends AI{
 				if (newPerk == Perk.BUCKET || newPerk == Perk.LAYERS && !turn.hasUnitAt(checkSpot)){
 					Base farthest = furthest(notOurBases, checkSpot);
 					int distance = farthest.position().distance(checkSpot);
-					if (distance > farthestDistance){
+					if (distance < farthestDistance){
 						farthestDistance = distance;
 						targetSpawn = checkSpot;
+						target = farthest;
 					}
 				}
 				// Check to make sure there is no one at that spawn point and that we are not already heading to that base
-				else if (!turn.hasUnitAt(checkSpot) && !targetBases.contains(nearest(notOurBases, checkSpot))){
+				else if (!turn.hasUnitAt(checkSpot)){
+					// Bugged Code: && !targetBases.contains(nearest(notOurBases, checkSpot))
 					// Find nearest base for this point
-					Base nearest = nearest(notOurBases, checkSpot);
+					Base nearest = nearest(difference(notOurBases,targetBases), checkSpot);
 					int distance = nearest.position().distance(checkSpot);
 					// Check to see if this is the shortest path
 					if (distance < shortestDistance){
 						shortestDistance = distance;
 						targetSpawn = checkSpot;
+						target = nearest;
 					}
 				}
 			}
+			
+			if (targetSpawn == null){
+				targetSpawn = any(spawns);
+				System.out.println("Failed to find spawn... selecting anything");
+			}
+			
+			System.out.println("Selected spawn pt.");
 
 			if (newPerk == Perk.BUCKET || newPerk == Perk.LAYERS){
 				// Aggressors
@@ -122,6 +133,8 @@ public class CompetitorAI extends AI{
 				// Passive
 				targetBases.add(nearest(notOurBases, targetSpawn));
 			}
+			
+			goals[unit_id] = target.position();
 
 
 			System.out.println("Spawning");
